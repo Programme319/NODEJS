c8c97d6902b1f98d0696f7a32220ee1a1381955c
Mohamed Gamal <106948663+Programme319@users.noreply.github.com>
2025-12-31 22:07:44 +0200

    inject express in project

---
+++ index.js
@@ -0,0 +1,25 @@
+const  fs = require("node:fs");
+const express = require('express');
+const app = express()
+
+// logging function to show request method, url and date-time
+function logger(req){
+    console.log(`${req.method}  ${req.url}  ${new Date()}`);
+}
+
+const HomePage = fs.readFileSync("./index.html", "utf-8"); //Home page as a variable string
+
+// routing using express
+app.get("/", (req, res) => {
+    logger(req);
+    res.send(HomePage);
+});
+app.get("/about", (req, res) => {
+    logger(req);
+    res.send("about page");
+});
+
+// the port to run the project
+app.listen('3001', () => {
+    console.log("server is listening on port 3000");
+});
