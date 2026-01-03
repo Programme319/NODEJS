386dc7278546c08271f7a2814555da793598441b
Mohamed Gamal <106948663+Programme319@users.noreply.github.com>
2026-01-04 00:09:07 +0200

    update and delete methods

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 app.use(express.json()); //middleware to parse json data
 const {body, validationResult} = require('express-validator');//importing validation functions that body for the send data and validationresult is for the massege
 
-const products_array=[ //instead of real API or database
+let products_array=[ //instead of real API or database
     {id:1, name:"laptop", price:45000},
     {id:2, name:"mobile", price:15000},
     {id:3, name:"laptop", price:45000},
@@ -13,7 +13,7 @@ const products_array=[ //instead of real API or database
 app.get('/products', (req, res) => {
     res.json(products_array);
 });
-
+// crud operations
 app.get('/products/:id', (req, res) => { //:id is a parameter in the url (dynamic/variable)
     const productId = +req.params.id;
     const product = products_array.find((product) => product.id === productId);//searching for product with id
@@ -41,6 +41,29 @@ app.post('/products',[  //validation middleware using express-validator
     const product = {id: products_array.length + 1, ...req.body};//creating new product object with unique id
     products_array.push(product);//pushing the new product to the main array
     res.status(201).json(product); //show the new element
+});
+
+//update product
+app.patch('/products/:id', (req, res) => {
+    let productId = +req.params.id;
+    let product = products_array.findIndex((product) => product.id === productId);
+    if(product === -1) {
+        return res.status(404).json({message: "product not found"});
+    }
+    product={...products_array[product], ...req.body};//updating the product with new data
+    products_array[product]=product;
+    res.json(product);
+});
+
+//delete product
+app.delete('/products/:id', (req, res) => {
+    let productId = +req.params.id;
+    let productIndex = products_array.findIndex((product) => product.id === productId);
+    if(productIndex === -1) {
+        return res.status(404).json({message: "product not found"});
+    }
+    products_array.splice(productIndex, 1);
+    res.json({message: "product deleted successfully"});
 });
 
 // the port to run the project
